e071064edd046ac4b105a84ab49629dcc6712698
yuki0509 <hara.0509@icloud.com>
2020-05-05 09:17:23 +0900

    finish CRUD

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,43 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save!
+    redirect_to root_path, notice: "タスク「#{@task.name}」を保存しました・"
+  end
+  
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update!(task_params)
+    redirect_to tasks_url, notice: "タスク「#{@task.name}」を更新しました。"
+  end
+  
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_url, notice: "タスク「#{@task.name}」を削除しました。"
+  end
+  
+
+  private
+    def task_params
+      params.require(:task).permit(:name, :description)
+    end
+  
+end
