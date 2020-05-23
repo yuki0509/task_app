d4ccb1e4dcc7b6d721430bf20f98d0f0bc82f006
yuki0509 <hara.0509@icloud.com>
2020-05-24 02:50:17 +0900

    mailer finish

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,16 +2,31 @@ class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   
   def index
-    @tasks = current_user.tasks.recent
+    @q = current_user.tasks.ransack(params[:q])
+    @tasks = @q.result(distinct: true)
   end
 
   def new
     @task = current_user.tasks.new
   end
 
+  def confirm_new
+    @task = current_user.tasks.new(task_params)
+    render :new unless @task.valid?
+  end
+
   def create
     @task = current_user.tasks.new(task_params)
+
+    if params[:back].present?
+      render :new
+      return
+    end
+    
+
     if @task.save
+      TaskMailer.creation_email(@task).deliver_now
+      logger.debug "task: #{@task.attributes.inspect}"
       redirect_to @task, notice: "タスク「#{@task.name}」を保存しました・"
     else
       render :new
